ddf3270f3966803ff68f63d20aed7fe1f05fa230
Camilo Soria <camilo.soria@uc.cl>
2020-05-05 10:41:52 -0400

    First Update

---
+++ futbol.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+driver = webdriver.Chrome("C:/Users/camil/OneDrive/Desktop/ProyectoFutbolJorge/venv/chromedriver.exe")
+driver.get("https://www.uefa.com/uefachampionsleague/fixtures-results/#/rd/2001141/2")
+first = driver.find_element_by_xpath('//*[@id="calendar-service_teams-wrapper-modal"]')
+first.click()
+real_m = driver.find_element_by_xpath('//*[@id="calendar-service_teams-wrapper-modal"]/div/div/div[2]/div/div[67]/input')
+juventus = driver.find_element_by_xpath('//*[@id="calendar-service_teams-wrapper-modal"]/div/div/div[2]/div/div[46]/input')
+f_barcelona = driver.find_element_by_xpath('//*[@id="calendar-service_teams-wrapper-modal"]/div/div/div[2]/div/div[18]/input')
+b_munich = driver.find_element_by_xpath('//*[@id="calendar-service_teams-wrapper-modal"]/div/div/div[2]/div/div[21]/input')
+man_city = driver.find_element_by_xpath('//*[@id="calendar-service_teams-wrapper-modal"]/div/div/div[2]/div/div[56]/input')
+teams = [real_m, juventus, b_munich, man_city]
+for team in teams:
+    team.click()
+add_calendar = driver.find_element_by_xpath('//*[@id="calendar-service_teams-wrapper-modal"]/div/div/div[3]/div/a')
+add_calendar.click()
+skip = driver.find_element_by_xpath('//*[@id="calendar-service-modal"]/div/div/a')
+skip.click()
+copy_link = driver.find_element_by_xpath('//*[@id="calendar-service-not_iosOrMac-modal"]/div/div/div[1]/div[2]/div/div[1]/a')
+copy_link.click()
+print(copy_link)
